d4caaa47375d97d3ad6ebf1e11161dfb86340c62
Bensly <bensly12@hotmail.com>
2015-04-07 12:26:16 -0500

    Added under construction

---
+++ js/communites.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Bensly on 4/2/2015.
+ */
+$(function() {
+    var items = $('#v-nav>ul>li').each(function () {
+        $(this).click(function () {
+
+            items.removeClass('current');
+            $(this).addClass('current');
+
+
+            $('#v-nav>div.tab-content').hide().eq(items.index($(this))).show('fast');
+
+        });
+    });
+
+});
