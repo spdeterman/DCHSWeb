ab414407b107f8b13ab315ff333603b4b284e305
Bensly <bensly12@hotmail.com>
2015-05-07 12:15:23 -0500

    Added under construction

--- js/communites.js
+++ js/communites.js
@@ -1,8 +1,18 @@
 /**
  * Created by Bensly on 4/2/2015.
  */
+(function(){
+    //alert("This is working.")
+
+    function hideAllButFirst(){
+        $('#v-nav>div.tab-content').hide();
+    }
+})();
+
 $(function() {
-    var items = $('#v-nav>ul>li').each(function () {
+    var items = $('#v-nav>ul>li').each(hideTabs);
+
+    function hideTabs() {
         $(this).click(function () {
 
             items.removeClass('current');
@@ -13,6 +23,6 @@ $(function() {
 
             window.location.hash = $(this).attr('tab');
         });
-    });
+    }
 
 });
